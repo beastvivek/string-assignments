a5b001c988cb1ac6c6ce49a712ea23718e4c484b
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-03-04 11:30:16 +0530

    Added test cases for uniqueLetterCount.js

--- uniqueLetterCount.js
+++ uniqueLetterCount.js
@@ -1,17 +1,28 @@
-const countUniqueLetter = function (string) {
+const countUniqueLetter = function (text) {
   let uniqueCharaters = "";
-  for (let index = 0; index < string.length; index++) {
-    if (!uniqueCharaters.includes(string[index])) {
-      uniqueCharaters += string[index];
+  for (let index = 0; index < text.length; index++) {
+    if (!uniqueCharaters.includes(text[index])) {
+      uniqueCharaters += text[index];
     }
   }
   return uniqueCharaters.length;
 }
 
-const main = function () {
-  console.log(countUniqueLetter("hello"));
-  console.log(countUniqueLetter("number"));
-  console.log(countUniqueLetter("11 unique letters"));
+const testCountUniqueLetters = function (text, expected, testDescription) {
+  let testResult = "✓";
+  if (countUniqueLetter(text) !== expected) {
+    testResult = "x";
+  }
+  console.log(testResult + " : " + testDescription);
 }
 
-main();
+const countUniqueLetterTestCases = function () {
+  testCountUniqueLetters("a", 1, "Only one character, which is unique");
+  testCountUniqueLetters("ab", 2, "Two characters word, where all character are unique");
+  testCountUniqueLetters("abc", 3, "Three characters word, where all character are unique");
+  testCountUniqueLetters("aa", 1, "Two characters word, where character repeats");
+  testCountUniqueLetters("aabb", 2, "Four characters word, with pair of unique character");
+  testCountUniqueLetters("", 0, "Empty text");
+}
+
+countUniqueLetterTestCases();
