d91f7fb513f672ab255a0903c06193014a99a563
Vivek Bisht <99522211+beastvivek@users.noreply.github.com>
2022-03-04 11:06:17 +0530

    Refactored test cases for palidromeString

--- palindromeString.js
+++ palindromeString.js
@@ -10,68 +10,20 @@ const isPalindrome = function (string) {
   return string === reverseString(string);
 }
 
-const main = function () {
-  console.log(isPalindrome("hello"));
-  console.log(isPalindrome("nitin"));
-  console.log(isPalindrome(""));
-}
-
-// main();
-
-const assertExpectation = function (actual, expected, testDescription) {
+const testIsPalindrome = function (text, expected, testDescription) {
   let testResult = "✓";
-  if (actual !== expected) {
+  if (isPalindrome(text) !== expected) {
     testResult = "x";
   }
-  return testResult + " : " + testDescription;
-}
-
-const testPalindromeString = function () {
-  const testDescription = "Should work for Palindrome String.";
-  const expected = true;
-  const actual = isPalindrome("nitin");
-
-  return assertExpectation(actual, expected, testDescription);
-}
-
-const testNonPalindromeString = function () {
-  const testDescription = "Should work for Non-Palindrome String.";
-  const expected = false;
-  const actual = isPalindrome("hello");
-
-  return assertExpectation(actual, expected, testDescription);
-}
-
-const testEmptyString = function () {
-  const testDescription = "Should work for Empty String.";
-  const expected = true;
-  const actual = isPalindrome("");
-
-  return assertExpectation(actual, expected, testDescription);
-}
-
-const testPalindromeNumericString = function () {
-  const testDescription = "Should work for Palindrome Numeric String.";
-  const expected = true;
-  const actual = isPalindrome("123321");
-
-  return assertExpectation(actual, expected, testDescription);
-}
-
-const testNonPalindromeNumericString = function () {
-  const testDescription = "Should work for Non Palindrome Numeric String.";
-  const expected = false;
-  const actual = isPalindrome("123421");
-
-  return assertExpectation(actual, expected, testDescription);
+  console.log(testResult + " : " + testDescription);
 }
 
 const isPalindromeTestCases = function () {
-  console.log(testPalindromeString());
-  console.log(testNonPalindromeString());
-  console.log(testEmptyString());
-  console.log(testPalindromeNumericString());
-  console.log(testNonPalindromeNumericString());
+  testIsPalindrome("a", true, "Single character");
+  testIsPalindrome("aa", true, "Two character Palindrome string");
+  testIsPalindrome("aba", true, "Three character Palindrome string");
+  testIsPalindrome("ab", false, "Two character Non-Palindrome string");
+  testIsPalindrome("", true, "Empty string");
 }
 
 isPalindromeTestCases();
